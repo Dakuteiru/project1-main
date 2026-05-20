f846afd0451b4899e812fcd249d8a8baf7822943
Evgenie <89211254600ek@gmail.com>
2026-05-20 21:19:31 +0300

    donate create & message of creation to product

--- app/Models/Donate.php
+++ app/Models/Donate.php
@@ -6,13 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-#[Fillable(['name', 'type', 'status', 'cost', 'gacha_id','user_id'])]
+#[Fillable(['name', 'type', 'status', 'cost', 'gacha_id', 'user_id'])]
 class Donate extends Model
 {
     public function gacha() : BelongsTo
     {
         return $this->belongsTo(Gacha::class);
     }
+
     public function users() : BelongsToMany
     {
         return $this->BelongsToMany(Gacha::class);
--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,12 @@ Route::get('/', function () {
 });
 
 Route::resource('products', ProductController::class);// resourse позволяет нам не писать пути ко всем контроллерам в productControllers а также не указывать тип их метода
-
-//Route::resource('donates', DonateController::class);
+// Route::resource('donates', DonateController::class);
 
 Route::get('donates',[DonateController::class,'index'])->name('donates.index');
 Route::post('donates',[DonateController::class, 'store'])->name('donates.store');
 Route::get('donates/create',[DonateController::class, 'create'])->name('donates.create');//create
 Route::get('donates/{donate}', [DonateController::class, 'show'])->name('donates.show');//show
 Route::get('donates/{donate}/edit',[DonateController::class, 'edit'])->name('donates.edit');//edit
-Route::post('donates/{donate}', [DonateController::class, 'update'])->name('donates.update');//update
+Route::put('donates/{donate}', [DonateController::class, 'update'])->name('donates.update');//update
 Route::delete('donates/{donate}',[DonateController::class,'destroy'])->name('donates.destroy');//destroy
